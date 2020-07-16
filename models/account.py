4f23c2ebd2ea756d3c13eae36e9d395312af82a0
jefferson <jeffer.leo93@gmail.com>
2020-07-16 10:42:41 -0600

    Proyecto no requerido

--- models/account.py
+++ models/account.py
@@ -71,74 +71,74 @@ class AccountMove(models.Model):
                         logging.warn(proyecto_dic[p])
                         proyecto_dic[p].write({'active':False})
 
-    # def post(self):
-    #     res = super(AccountMove, self).post()
-    #     for factura in self:
-    #         if factura.type in ['in_receipt','in_invoice']:
-    #             suma_porcentaje = 0
-    #             lineas_proyectos = []
-    #             if factura.proyecto_ids:
-    #                 for linea_proyecto in factura.proyecto_ids:
-    #                     suma_porcentaje += linea_proyecto.porcentaje
-    #                     proy = {"_id": str(linea_proyecto.proyecto_id.id_blautech),"distributionType": "PERCENTAGE","distributionValue": str(linea_proyecto.porcentaje)}
-    #                     lineas_proyectos.append(proy)
-    #             else:
-    #                 raise UserError(str('FAVOR DE INGRESAR POR LO MENOS UN PROYECTO O CENTRO DE COSTO'))
-    #
-    #             if suma_porcentaje != 100:
-    #                 raise UserError(str('EL PORCENTAJE DE PROYECTOS/CENTROS DE COSTOS NO PUEDE SER DIFERENTE DE 100%'))
-    #             else:
-    #                 logging.warn(factura.invoice_date)
-    #                 logging.warn(factura.invoice_sequence_number_next)
-    #                 logging.warn(factura.invoice_sequence_number_next_prefix)
-    #                 logging.warn(factura.name)
-    #                 factura_proveedor_body={"projectDistribution": lineas_proyectos,
-    #                         "totalAmount": str(factura.amount_total),
-    #                         "currency": str(factura.currency_id.name),
-    #                         "vendor": str(factura.partner_id.name),
-    #                         "purchaseOrder": str(factura.name),
-    #                         "purchaseDate": str(factura.invoice_date),
-    #                         "datail": {
-    #                             "campo": "valor"
-    #                         },
-    #                         "createdBy": "Odoo"
-    #                     }
-    #                 url = "https://kykfm3bqpj.execute-api.us-east-1.amazonaws.com/dev/api/purchase"
-    #                 headers = {
-    #                     "x-api-key": "1qy0D4R4b39BOmRC8nOon5Umoa54aAJh5nmDuwSA",
-    #                     'Content-type': 'application/json',
-    #                     'Accept': 'text/plain'
-    #                 }
-    #
-    #                 auth = HTTPBasicAuth('x-api-key', '1qy0D4R4b39BOmRC8nOon5Umoa54aAJh5nmDuwSA')
-    #
-    #                 response = requests.post(url, json = factura_proveedor_body, headers=headers,verify=False)
-    #                 r = response.json()
-    #                 logging.warn(response.json())
-    #                 logging.warn(response)
-    #                 if r['success']:
-    #                     factura.id_blautech = r['result']['_id']
-    #                     return res
-    #                 else:
-    #                     raise UserError(str(r['message']))
-    #
-    # def button_draft(self):
-    #     res = super(AccountMove, self).button_draft()
-    #     for factura in self:
-    #         if factura.id_blautech:
-    #             url = "https://kykfm3bqpj.execute-api.us-east-1.amazonaws.com/dev/api/purchase/" + str(factura.id_blautech)
-    #             headers = {
-    #                 "x-api-key": "1qy0D4R4b39BOmRC8nOon5Umoa54aAJh5nmDuwSA",
-    #                 'Content-type': 'application/json',
-    #                 'Accept': 'text/plain'
-    #             }
-    #             response = requests.delete(url,headers=headers)
-    #             logging.warn(response)
-    #             r = response.json()
-    #             logging.warn(r)
-    #
-    #             if r['success']:
-    #                 factura.id_blautech = ""
-    #                 return res
-    #             else:
-    #                 raise UserError("Compra no existente en ZOHO")
+    def post(self):
+        res = super(AccountMove, self).post()
+        for factura in self:
+            if factura.type in ['in_receipt','in_invoice'] and factura.proyecto_ids:
+                suma_porcentaje = 0
+                lineas_proyectos = []
+                if factura.proyecto_ids:
+                    for linea_proyecto in factura.proyecto_ids:
+                        suma_porcentaje += linea_proyecto.porcentaje
+                        proy = {"_id": str(linea_proyecto.proyecto_id.id_blautech),"distributionType": "PERCENTAGE","distributionValue": str(linea_proyecto.porcentaje)}
+                        lineas_proyectos.append(proy)
+                else:
+                    raise UserError(str('FAVOR DE INGRESAR POR LO MENOS UN PROYECTO O CENTRO DE COSTO'))
+
+                if suma_porcentaje != 100:
+                    raise UserError(str('EL PORCENTAJE DE PROYECTOS/CENTROS DE COSTOS NO PUEDE SER DIFERENTE DE 100%'))
+                else:
+                    logging.warn(factura.invoice_date)
+                    logging.warn(factura.invoice_sequence_number_next)
+                    logging.warn(factura.invoice_sequence_number_next_prefix)
+                    logging.warn(factura.name)
+                    factura_proveedor_body={"projectDistribution": lineas_proyectos,
+                            "totalAmount": str(factura.amount_total),
+                            "currency": str(factura.currency_id.name),
+                            "vendor": str(factura.partner_id.name),
+                            "purchaseOrder": str(factura.name),
+                            "purchaseDate": str(factura.invoice_date),
+                            "datail": {
+                                "campo": "valor"
+                            },
+                            "createdBy": "Odoo"
+                        }
+                    url = "https://kykfm3bqpj.execute-api.us-east-1.amazonaws.com/dev/api/purchase"
+                    headers = {
+                        "x-api-key": "1qy0D4R4b39BOmRC8nOon5Umoa54aAJh5nmDuwSA",
+                        'Content-type': 'application/json',
+                        'Accept': 'text/plain'
+                    }
+
+                    auth = HTTPBasicAuth('x-api-key', '1qy0D4R4b39BOmRC8nOon5Umoa54aAJh5nmDuwSA')
+
+                    response = requests.post(url, json = factura_proveedor_body, headers=headers,verify=False)
+                    r = response.json()
+                    logging.warn(response.json())
+                    logging.warn(response)
+                    if r['success']:
+                        factura.id_blautech = r['result']['_id']
+                        return res
+                    else:
+                        raise UserError(str(r['message']))
+
+    def button_draft(self):
+        res = super(AccountMove, self).button_draft()
+        for factura in self:
+            if factura.id_blautech:
+                url = "https://kykfm3bqpj.execute-api.us-east-1.amazonaws.com/dev/api/purchase/" + str(factura.id_blautech)
+                headers = {
+                    "x-api-key": "1qy0D4R4b39BOmRC8nOon5Umoa54aAJh5nmDuwSA",
+                    'Content-type': 'application/json',
+                    'Accept': 'text/plain'
+                }
+                response = requests.delete(url,headers=headers)
+                logging.warn(response)
+                r = response.json()
+                logging.warn(r)
+
+                if r['success']:
+                    factura.id_blautech = ""
+                    return res
+                else:
+                    raise UserError("Compra no existente en ZOHO")
