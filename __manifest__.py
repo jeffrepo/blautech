d8468cebbc1159bdef47ee39ec18f34f43d0de5f
jeffrepo <jeffer.leo93@gmail.com>
2026-03-02 13:25:24 -0600

    19.0

--- __manifest__.py
+++ __manifest__.py
@@ -8,20 +8,20 @@
         Customs para Blautech
     """,
 
-    'author': "JS",
+    'author': "Silva technologies",
     'website': "",
 
     'category': 'Uncategorized',
-    'version': '0.1',
+    'version': '1.0',
 
     'depends': ['account','base', 'account_accountant', 'account_gt'],
 
     'data': [
         'security/blautech_groups.xml',
         'views/menu_item_views.xml',
-        'views/account_move_views.xml',
+        #'views/account_move_views.xml',
         # 'security/ir.model.access.csv',
-        'views/blautech_presupuesto_wizard_view.xml',
+        #'views/blautech_presupuesto_wizard_view.xml',
 
     ],
 }
